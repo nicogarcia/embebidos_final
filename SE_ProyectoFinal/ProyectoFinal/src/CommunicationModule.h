44d47c978882774a9ac9475849897bab69256725
federicojasson <federicojasson@gmail.com>
2014-06-23 16:57:36 -0300

    Removed commented code in CommunicationModule.h

--- SE_ProyectoFinal/ProyectoFinal/src/CommunicationModule.h
+++ SE_ProyectoFinal/ProyectoFinal/src/CommunicationModule.h
@@ -28,20 +28,4 @@ private:
     static void sendMessage(String message);
 };
 
-/*class CommunicationModule {
-public:
-    static SoftwareSerial BTSerial;
-    static void bluetoothEvent();
-    static void readMessage();
-    static void sendErrorResponse(Parameter error_parameter);
-    static void sendLoginResponse(Parameter logged_in_parameter);
-    static void sendRequestStateResponse(Parameter lock_closed_parameter, Parameter light_off_parameter, Parameter light_disabled_parameter, Temperature temperature, Humidity humidity);
-    static void sendRequestUsersResponse(int user_count, Username usernames[USER_TABLE_CAPACITY]);
-    static void sendSuccessResponse();
-private:
-    static void sendMessage(String message);
-    static void my_strcpy(const char* source, char* destiny);
-};*/
-
-
 #endif /* COMMUNICATION_MODULE */
